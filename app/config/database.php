9f24327b4abf98a1eae3ac77bea2f5ba59dfab15
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2013-09-20 17:52:13 -0500

    Configuring database.

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'tutorias',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'toor',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
