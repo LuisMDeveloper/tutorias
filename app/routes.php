cb61e2228ac1a53e81b8b72f78c41c9b307174bd
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2013-09-20 17:59:15 -0500

    Modifying routes.

--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,28 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+
+	//$docentesSistemas = Carrera::find(1)->docentes;
+
+	//dd($docentesSistemas);
+
+	//$docente = Docente::find(1)->carrera;
+
+	//dd($docente);
+
+	$docente = Docente::where('carrera_id', '=', null)->get();
+
+	dd($docente);
+
+
+	return View::make('layouts.master')->with(array(
+		'title' => 'Tutorias'
+	));
+});
+
+Route::get('singin', function()
+{
+	return View::make('singin')->with(array(
+		'title' => 'Tutorias'
+	));
 });
